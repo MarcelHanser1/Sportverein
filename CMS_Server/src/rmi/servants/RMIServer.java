37832361fbee6c4cdd55144dc08293bd850b64e0
Sevia2000 <severin1992@gmx.ch>
2018-11-05 17:42:39 +0100

    Additional IP address for Lukas!

--- CMS_Server/src/rmi/servants/RMIServer.java
+++ CMS_Server/src/rmi/servants/RMIServer.java
@@ -3,7 +3,9 @@ package rmi.servants;
 import org.hibernate.SessionFactory;
 import persistence.HibernateUtil;
 
+import java.net.DatagramSocket;
 import java.net.InetAddress;
+import java.net.SocketException;
 import java.net.UnknownHostException;
 import java.rmi.Naming;
 import java.rmi.registry.LocateRegistry;
@@ -18,7 +20,7 @@ public class RMIServer {
         try {
             SessionFactory _sessionFactory = HibernateUtil.getSessionFactory();
             rmiHostName = InetAddress.getLocalHost().getHostAddress();
-            System.out.println("\nIP Address: " + InetAddress.getLocalHost().getHostAddress() +"\n");
+            System.out.println("\nServer IP Address: " + InetAddress.getLocalHost().getHostAddress() +"\n");
         } catch (UnknownHostException e) {
             e.printStackTrace();
         }
@@ -27,11 +29,24 @@ public class RMIServer {
     // RMI URL
     private static String rmiURL;
 
+    private static void initializeIP(){
+        try(final DatagramSocket socket = new DatagramSocket()){
+            socket.connect(InetAddress.getByName("8.8.8.8"), 10002);
+            String ip = socket.getLocalAddress().getHostAddress();
+            System.out.println("\nLukas IP Address: " + ip + "\n");
+        } catch (SocketException e) {
+            e.printStackTrace();
+        } catch (UnknownHostException e){
+            e.printStackTrace();
+        }
+    }
     public static void main(String[] args){
 
         try{
-
             Registry rmiRegistry = LocateRegistry.createRegistry(Registry.REGISTRY_PORT);
+
+            initializeIP();
+
             // set the policy file
             // alternative:
             // add this line to the Run/Debug Configurations VM:
@@ -48,9 +63,10 @@ public class RMIServer {
             System.out.println("\nAll bound Objects:");
             Stream.of(rmiRegistry.list()).forEach(x -> System.out.println(x));
 
+            System.out.println("\n");
+
         } catch (Exception ex){
             ex.printStackTrace();
         }
     }
-
 }
