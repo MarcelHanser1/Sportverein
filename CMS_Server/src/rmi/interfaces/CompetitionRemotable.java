e7a0111e9c6a437f4010d9c3fd69e1dc529ff8c3
marcel <windbreaker5@hotmail.com>
2018-11-05 22:34:56 +0100

    Update CMS_Server/src/rmi/interfaces/CompetitionRemotable.java

--- CMS_Server/src/rmi/interfaces/CompetitionRemotable.java
+++ CMS_Server/src/rmi/interfaces/CompetitionRemotable.java
@@ -9,26 +9,26 @@ import java.util.List;
 
 public interface CompetitionRemotable extends Remote {
 
-    void addNewCompetition (CompetitionDTO competition) throws RemoteException;
+    void addNewCompetition(CompetitionDTO competition) throws RemoteException;
 
 
 
 
-    public List<CompetitionDTO> getAllCompetitions();
+    public List<CompetitionDTO> getAllCompetitions()  throws RemoteException;;
 
-    public CompetitionDTO addTeamsToCompetitionDTO (CompetitionDTO competitionDTO);
+    public CompetitionDTO addTeamsToCompetitionDTO(CompetitionDTO competitionDTO)  throws RemoteException;;
 
-    public List<TeamDTO> getTeamsfromCompetitionDto(CompetitionDTO competitionDTO);
+    public List<TeamDTO> getTeamsfromCompetitionDto(CompetitionDTO competitionDTO)  throws RemoteException;;
 
-    public void updateCompetition (CompetitionDTO competitionDTO);
+    public void updateCompetition(CompetitionDTO competitionDTO)  throws RemoteException;;
 
-    public void insertCompetition (CompetitionDTO competitionDTO);
+    public void insertCompetition(CompetitionDTO competitionDTO)  throws RemoteException;;
 
-    public void addTeam (CompetitionDTO competitionDTO);
+    public void addTeam(CompetitionDTO competitionDTO)  throws RemoteException;;
 
-    public void deleteCompetition(CompetitionDTO competitionDTO);
+    public void deleteCompetition(CompetitionDTO competitionDTO)  throws RemoteException;;
 
-    public List<TeamDTO> getAllTeams();
+    public List<TeamDTO> getAllTeams()  throws RemoteException;;
 
 
 }
