b9f7f74760309dca84984fa4e757d99ced078933
lukas-v95 <lukas1995@nurfuerspam.de>
2018-11-06 17:32:16 +0100

    working rmiURL

--- CMS_Server/src/rmi/servants/RMIServer.java
+++ CMS_Server/src/rmi/servants/RMIServer.java
@@ -3,9 +3,7 @@ package rmi.servants;
 import org.hibernate.SessionFactory;
 import persistence.HibernateUtil;
 
-import java.net.DatagramSocket;
 import java.net.InetAddress;
-import java.net.SocketException;
 import java.net.UnknownHostException;
 import java.rmi.Naming;
 import java.rmi.registry.LocateRegistry;
@@ -13,40 +11,22 @@ import java.rmi.registry.Registry;
 import java.util.stream.Stream;
 
 public class RMIServer {
-
-    private static String rmiHostName;
-
-    static {
-        try {
-            System.out.println("");
-            SessionFactory _sessionFactory = HibernateUtil.getSessionFactory();
-            rmiHostName = InetAddress.getLocalHost().getHostAddress();
-            System.out.println("\nServer IP Address: " + InetAddress.getLocalHost().getHostAddress() +"\n");
-        } catch (UnknownHostException e) {
-            e.printStackTrace();
-        }
-    }
-
     // RMI URL
     private static String rmiURL;
 
-    private static void initializeIP(){
-        try(final DatagramSocket socket = new DatagramSocket()){
-            socket.connect(InetAddress.getByName("8.8.8.8"), 10002);
-            String ip = socket.getLocalAddress().getHostAddress();
-            System.out.println("\nLukas IP Address: " + ip + "\n");
-        } catch (SocketException e) {
-            e.printStackTrace();
-        } catch (UnknownHostException e){
-            e.printStackTrace();
-        }
+
+    private static void initIP() throws UnknownHostException {
+            SessionFactory _sessionFactory = HibernateUtil.getSessionFactory();
+            rmiURL = "rmi://" + InetAddress.getLocalHost().getHostAddress() + "/";
+            System.out.println("\nServer IP Address: " + InetAddress.getLocalHost().getHostAddress() + "\n");
+
     }
-    public static void main(String[] args){
 
-        try{
+    public static void main(String[] args) {
+
+        try {
+            initIP();
             Registry rmiRegistry = LocateRegistry.createRegistry(Registry.REGISTRY_PORT);
-
-            initializeIP();
 
             // set the policy file
             // alternative:
@@ -55,7 +35,6 @@ public class RMIServer {
             // System.setProperty("java.security.policy", "file:./server.policy");
 
 
-            rmiURL = "rmi://" + rmiHostName + "/";
             Factory factory = new Factory();
             Naming.rebind(rmiURL + "Factory", factory);
             // Confirmation message!
@@ -66,7 +45,7 @@ public class RMIServer {
 
             System.out.println("\n");
 
-        } catch (Exception ex){
+        } catch (Exception ex) {
             ex.printStackTrace();
         }
     }
